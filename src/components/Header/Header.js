2d20365b571194a3e6fa7417884397c52c8da022
Mario Andres <marioarendon2@gmail.com>
2022-08-16 18:21:15 -0600

    centered nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Logo />
+        <Side>
+          <Logo />
+        </Side>
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -21,28 +23,23 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
+        <Side />
       </MainHeader>
     </header>
   );
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 16px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
   display: flex;
   align-items: baseline;
   justify-content: flex-start;
-  padding: 16px 32px;
-
-  & > a {
-    margin-right: 100px;
-  }
 `;
 
 const Nav = styled.nav`
   display: flex;
   justify-content: space-evenly;
-  width: 1000px;
 `;
 
 const NavLink = styled.a`
@@ -51,11 +48,15 @@ const NavLink = styled.a`
   text-decoration: none;
   color: ${COLORS.gray[900]};
   font-weight: ${WEIGHTS.medium};
-  margin-right: 48px;
+  margin: 0 48px;
 
   &:first-of-type {
     color: ${COLORS.secondary};
   }
+`;
+
+const Side = styled.div`
+  flex: 1;
 `;
 
 export default Header;
